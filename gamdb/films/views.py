f153a583eb74bea6fd05dbf59c9714af7146a955
Matěj Bittner <matej.bittner@student.gyarab.cz>
2023-03-28 11:20:40 +0200

    template

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
 
-# Create your views here.
+def homepage(request):
+    context = {
+        
+        'movies': Movie.objects.all(),
+    }
+    return render(request, 'main.html', context)
+    #return HttpResponse("ahoj")
