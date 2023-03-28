eeeb28c4e46bb0270250354d056d896262f1fcf4
Matěj Bittner <matej.bittner@student.gyarab.cz>
2023-03-28 10:00:57 +0200

    director and genres

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 
-from .models import Movie
+from .models import Movie, Director, Genre
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'year']
+    list_display = ['id', 'name', 'director', 'year', 'genres_display']
     list_display_links = ['name']
     search_fields = ['name', 'description']
+    list_filter = ['genres']
 
+
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'birth_year']
+    list_display_links = ['name']
+    search_fields = ['name', 'birth_year']
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+    search_fields = ['name']
 
 admin.site.register(Movie, MovieAdmin)
-
+admin.site.register(Director, DirectorAdmin)
+admin.site.register(Genre, GenreAdmin)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -2,11 +2,36 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=200)
-    year = models.IntegerField()
+    year = models.IntegerField(blank=True, null=True)
     description = models.TextField()
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    genres = models.ManyToManyField('Genre', blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.year})"
+    def genres_display(self):
+        return ", ".join([i.name for i in self.genres.all()])
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=200)
+    birth_year = models.IntegerField(blank=True, null=True)
+    
+
+    def __str__(self):
+        return f"{self.name} ({self.birth_year})"
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+"py -3 -m venv venv"
+"django-admin startproject"
+"              startapp"
+
 
 "python manage.py makemigrations            - připraví migrační script"
 "python manage.py migrate"
@@ -15,5 +40,5 @@ class Movie(models.Model):
 "python manage.py runserver"
 "                 dumpdata -format yaml  films>films.yaml"
 "                 loaddata films.yaml"
-
+"                          fixtures/* "
 " python manage.py   .............   ./manage.py"
